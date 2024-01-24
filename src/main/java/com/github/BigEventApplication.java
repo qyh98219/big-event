61a43fc506230d6d2d5f5906e8297f8c13143b6a
admin <admin@szce.com>
2024-01-24 10:04:37 +0800

    添加项目所需依赖，调整项目结构，添加实体类

--- src/main/java/com/github/BigEventApplication.java
+++ src/main/java/com/github/BigEventApplication.java
@@ -1,13 +1,18 @@
-package com.github.bigevent;
+package com.github;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author qyh
+ * @Description 主程序启动类
+ * @Date 2024/1/24 9:49
+ */
 @SpringBootApplication
+@MapperScan("com.github.mapper")
 public class BigEventApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BigEventApplication.class, args);
     }
-
 }
